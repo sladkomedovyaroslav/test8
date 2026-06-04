99e5e65cb6364912deb0b3d47ce34b16c83f4112
sladkomedovyaroslav <sladkomedov.yaroslav10@gmail.com>
2026-06-04 20:16:29 +0300

    Create db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+function connectDB()
+{
+    return new PDO(
+        'mysql:host=localhost;dbname=restaurant_app;charset=utf8mb4',
+        'root',
+        '',
+        [
+            PDO::ATTR_ERRMODE =>
+                PDO::ERRMODE_EXCEPTION,
+
+            PDO::ATTR_DEFAULT_FETCH_MODE =>
+                PDO::FETCH_ASSOC
+        ]
+    );
+}
